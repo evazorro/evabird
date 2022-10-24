b5fbed201ff90d85adcee86aef2321aa9b2be90a
Eva Parish <eparish@squarespace.com>
2022-10-24 16:22:40 -0400

    Add requests library, eBird API key, notable birds function, example template

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+import re
+from flask import Flask, render_template
+from get_recent_notable_birds import get_recent_notable_birds
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello, eBird!"
+def index():
+    birds = get_recent_notable_birds()
+    return next(iter(birds))
+
+@app.route('/<name>')
+def hello_world(name):
+    return render_template('hello.html.j2', name=name)
---
+++ get_recent_notable_birds.py
@@ -0,0 +1,67 @@
+# Uses Cornell's eBird API
+# Docs: https://documenter.getpostman.com/view/664302/S1ENwy59?version=latest
+
+
+import requests
+
+
+def get_list_of_common_names(response):
+    """Parses a list of common names for birds from a JSON response
+    
+    The JSON response should contain a key, "comName," that contains the common name of the bird.
+
+    Args:
+        response: The JSON response
+
+    Returns:
+        A list containing the common names of all birds in the JSON response
+
+    Raises:
+        KeyError: If the JSON response does not have the "comName" key
+    """
+    birds = response.json()
+    birds_list = []
+    for bird in birds:
+        birds_list.append(bird["comName"])
+    return birds_list
+
+# Cast to a set to remove duplicates
+def get_unique_common_names(birds_list):
+    """Casts a list of bird names to a set to remove duplicate names
+    
+    Args:
+        birds_list: A list containing common names of birds
+
+    Returns:
+        A set containing common names of birds
+    """
+    return(set(birds_list))
+
+# Main
+def get_recent_notable_birds():
+    """Hits eBird's "Recent notable observations in a region" API for a hardcoded region.
+
+    From eBird: "Get the list of recent, notable observations (up to 30 days ago) of birds seen in a country, region or location.
+
+    Notable observations can be for locally or nationally rare species or are otherwise unusual, e.g. over-wintering birds in a species which is normally only a summer visitor."
+    
+    https://documenter.getpostman.com/view/664302/S1ENwy59#397b9b8c-4ab9-4136-baae-3ffa4e5b26e4
+
+    Returns:
+        A set containing names of birds
+    """
+
+    LOCATION_ID = 'US-NY-061' # New York, NY
+
+    with open('key.txt') as f:
+        EBIRD_API_KEY = f.read()
+    url = 'https://api.ebird.org/v2/data/obs/' + LOCATION_ID + '/recent/notable'
+    header = {'X-eBirdApiToken': EBIRD_API_KEY}
+
+    r = requests.get(url, headers=header)
+    recent_notable_birds = get_unique_common_names(get_list_of_common_names(r))
+
+    # for bird in recent_notable_birds:
+    #     print(bird)
+
+    return recent_notable_birds
